e450e4e85e9e331608e1d50a790cef07ccf7767d
Henrietta Obioha <HenriettaObioha1@gmail.com>
2023-08-09 04:20:08 -0700

    models

---
+++ models/amenity.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Defines Amenity Class"""
+from .base_model import BaseModel
+
+class Amenity(BaseModel):
+    """Contains class-specific attribute"""
+    name = ""
---
+++ models/city.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This creates a user class"""
+from models.base_model import BaseModel
+
+class City(BaseModel):
+    """Class for managing this city objects"""
+
+    state_id = ""
+    name = ""
